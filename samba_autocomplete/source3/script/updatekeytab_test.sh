06d8273a223313eed5e0209ecc1fbaa1479c297d
RabiOpensource <146856454+RabiOpensource@users.noreply.github.com>
2025-04-16 20:05:08 +0530

    Delete samba_autocomplete directory

--- samba_autocomplete/source3/script/updatekeytab_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
- ./ctdb/tests/local_daemons.sh "$PREFIX_ABS/clusteredmember" onnode all 'net ads keytab create --option="sync machine password script=" --configfile=$CTDB_BASE/lib/server.conf'
